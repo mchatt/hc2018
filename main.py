d6f3e76d0560c4e0f2f07d1504c29adc3088f92f
Mohamed Amine Chatt <mohamed-amine.chatt@amadeus.com>
2018-03-01 19:37:32 +0100

    new classes

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import sys
 import argparse
 import time
 
+class Car:
+  def __init__(self):
+    self.free = True
+    self.position = [0,0]
+    self.currentRide = None
+    self.list_rides = []
+
 class Ride:
   def __init__(self, n, a, b, x, y, t1, t2):
     self.n = n
@@ -19,7 +26,7 @@ class Input:
   def __init__(self):
     self.R = 0
     self.C = 0
-    self.vehicules = 0
+    self.nb_car = 0
     self.nb_rides = 0
     self.bonus = 0
     self.step = 0
@@ -28,7 +35,7 @@ class Input:
   def parse(self, inputFile):
     with open(inputFile) as f:
       content = f.readlines()
-      self.R, self.C, self.vehicules, self.nb_rides, self.bonus, self.step = [int(x) for x in content[0].split('\n')[0].split()]
+      self.R, self.C, self.nb_car, self.nb_rides, self.bonus, self.step = [int(x) for x in content[0].split('\n')[0].split()]
       for i in range(self.nb_rides):
         tmp = list(content[i].split('\n')[0])
         self.rides.append(Ride(i, tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5]))
@@ -40,26 +47,25 @@ class Input:
       p += str(x) + '\n'
     return '%d %d %d %d\n%s' % (self.R, self.C, self.L, self.H, p)
 
+# class Output:
+#   '''
+#   Class representing the output data
+#   '''
+#   def __init__(self):
+#     self.paires = [] #list of slices
 
-class Output:
-  '''
-  Class representing the output data
-  '''
-  def __init__(self):
-    self.paires = [] #list of slices
+#   def __str__(self):
+#     p = 'print Output {%d}\n' % len(self.paires)
+#     for x in self.paires:
+#       p += str(x) + '\n'
+#     return p
 
-  def __str__(self):
-    p = 'print Output {%d}\n' % len(self.paires)
-    for x in self.paires:
-      p += str(x) + '\n'
-    return p
-
-  def formatOutput(self, outputFile):
-    f = open(outputFile, 'w')
-    f.write('%d\n' % len(self.paires))
-    for p in self.paires :
-      f.write(' '.join([str(x) for x in p]))
-    f.close()
+#   def formatOutput(self, outputFile):
+#     f = open(outputFile, 'w')
+#     f.write('%d\n' % len(self.paires))
+#     for p in self.paires :
+#       f.write(' '.join([str(x) for x in p]))
+#     f.close()
 
 
 ###################################################################################################
@@ -69,34 +75,13 @@ def compute(inp):
   '''
   Implementation of the computing logic
   '''
-  res = Output()
+  for i in range(inp.step):
+    print(i)
+    # move the car in the map
 
-  def findSlices(inp, startingPoint, slices):
-    print('startingPoint', startingPoint)
-    print('slices', slices)
-    if startingPoint == [inp.R-1, inp.C-1]:
-      return slices
-    m = 0
-    t = 0
-    for i in range(inp.R-startingPoint[0]):
-      for j in range(2*i+1):
-        l = i+startingPoint[0] if (j <= i+1) else 2*i-j
-        c = j+startingPoint[1] if (j <= i+1) else i+1
-        if l >= inp.R or c >= inp.C:
-          continue
-        if inp.M[l][c] == 'M':
-          m = m+1
-      else:
-          t = t+1
-      if m >= inp.L and m >= inp.L:
-          return findSlices(inp, [startingPoint[0],c+1], slices + [[startingPoint, [l, c]]])
+    # if the car arrived to it destination, turn the free flag to True
 
-    return slices
-
-  res.paires = findSlices(inp, [0,0], [])
-
-  return res
-
+    # assign a ride to a vehicle
 
 
 ####################################################################################################
